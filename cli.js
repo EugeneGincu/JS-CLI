b9cd4dae0361746a3fc91358baf8c39fc5d2da23
Eugene Gincu <eugene.gincu@gmail.com>
2026-03-20 23:54:03 -0600

    created task adding function prototype

--- cli.js
+++ cli.js
@@ -1,13 +1,32 @@
 "use strict"
+
 let arg;
 let tasks = []
 
+console.log(tasks.length);
 
 if (!(arg = process.argv[2])) return;
 
 console.log("Hi");
 
 
+
+switch (arg) {
+	case "add":
+	console.log("adding");
+	let task = {};
+	task.id = tasks.length;
+	task.description = "temp";
+	task.status = "todo";
+	task.createdAt = new Date();
+	tasks.push(task);
+	break;
+	default:
+	console.log("Unrecognized command!");
+	break;
+}
+
+console.log(tasks.length);
 
 function hello(greeting) {
     return greeting;
