4eab02c54e6bd49264fa3b390bd1dbf98e634b6e
Eugene <eugene.gincu@gmail.com>
2026-03-21 23:53:05 -0600

    Successfully write task to file

--- cli.js
+++ cli.js
@@ -1,32 +1,69 @@
 "use strict"
 
+const fs= require('node:fs');
 let arg;
-let tasks = []
 
-console.log(tasks.length);
+
+//console.log(tasks.length);
 
 if (!(arg = process.argv[2])) return;
 
 console.log("Hi");
 
-
-
 switch (arg) {
 	case "add":
+	const data = readFile();
+	let tasks = [];
+	
+	if (!tasks) break;
+	
 	console.log("adding");
 	let task = {};
 	task.id = tasks.length;
-	task.description = "temp";
-	task.status = "todo";
+	task.description = process.argv[3];
+	task.status = process.argv[4];
 	task.createdAt = new Date();
 	tasks.push(task);
+	writeFile(tasks);
 	break;
+
+	case "update":
+	
+	break;
+
+	case "list":
+	if (!tasks.length)
+		console.log("Number of tasks", tasks.length);
+	break;
+
+	
+
 	default:
 	console.log("Unrecognized command!");
 	break;
 }
 
-console.log(tasks.length);
+
+//console.log(stringified);
+
+function readFile() {
+	try {
+		let data = fs.readFileSync('tasks.txt', 'utf8');
+		return data;
+	} catch (err) {
+		console.error(err);
+		return;
+	}
+}
+
+function writeFile(data) {
+	try {
+		let stringified = JSON.stringify(data);
+		fs.writeFileSync('tasks.txt',stringified);
+	} catch (err) {
+		console.error(err);
+	}
+}
 
 function hello(greeting) {
     return greeting;
