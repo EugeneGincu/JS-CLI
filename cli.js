2bb2c55d3f98eb26b6442cb8e22779957db309e8
Eugene <eugene.gincu@gmail.com>
2026-03-23 23:55:48 -0600

    fixed faulty commit, fixed reading file

--- cli.js
+++ cli.js
@@ -1,11 +1,15 @@
 "use strict"
 
-const open = require('open');
-
+const fs= require('node:fs');
 let arg;
-let tasks = []
+let tasks;
+const data = readFile();
+if (!data)
+	tasks = [];
+else
+	tasks = data;
 
-console.log(tasks.length);
+//console.log(tasks.length);
 
 if (!(arg = process.argv[2])) return;
 
@@ -13,6 +17,7 @@ console.log("Hi");
 
 switch (arg) {
 	case "add":
+	
 	console.log("adding");
 	let task = {};
 	task.id = tasks.length;
@@ -20,6 +25,7 @@ switch (arg) {
 	task.status = process.argv[4];
 	task.createdAt = new Date();
 	tasks.push(task);
+	writeFile(tasks);
 	break;
 
 	case "update":
@@ -27,17 +33,38 @@ switch (arg) {
 	break;
 
 	case "list":
-	//if (!tasks.length)
-	//	console.log("Number of tasks", tasks.length);
+	console.log("Number of tasks", tasks.length);
 	break;
+
+	
 
 	default:
 	console.log("Unrecognized command!");
 	break;
 }
 
-let stringified = JSON.stringify(tasks);
-console.log(stringified);
+
+//console.log(stringified);
+
+function readFile() {
+	try {
+		let data = fs.readFileSync('tasks.txt', 'utf8');
+		data = JSON.parse(data);
+		return data;
+	} catch (err) {
+		console.error(err);
+		return;
+	}
+}
+
+function writeFile(data) {
+	try {
+		let stringified = JSON.stringify(data);
+		fs.writeFileSync('tasks.txt',stringified);
+	} catch (err) {
+		console.error(err);
+	}
+}
 
 function hello(greeting) {
     return greeting;
