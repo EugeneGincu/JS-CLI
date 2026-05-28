d7236d7bde70c969527bc9a12a6bc4786c70da3e
Eugene <eugene.gincu@gmail.com>
2026-05-28 00:14:02 -0600

    updated date script

--- date.js
+++ date.js
@@ -4,3 +4,9 @@ const today = new Date();
 
 console.log(today.toLocaleDateString());
 console.log(today.toLocaleTimeString());
+
+const date1 = new Date("October 1, 2025");
+const date2 = new Date(2024, 9, 1, 13, 10, 30);
+
+console.log(date1.toLocaleDateString());
+console.log(date2.toLocaleDateString(), date2.toLocaleTimeString());
