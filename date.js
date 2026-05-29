6c57abcd569e297d6557dcafa4aa48f43b253582
Eugene <eugene.gincu@gmail.com>
2026-05-28 23:27:07 -0800

    added functionality to date.js cli

--- date.js
+++ date.js
@@ -1,5 +1,9 @@
 "use strict"
 
+let month = process.argv[2];
+let date = process.argv[3];
+let year = process.argv[4];
+
 const today = new Date();
 
 console.log(today.toLocaleDateString());
@@ -10,3 +14,8 @@ const date2 = new Date(2024, 9, 1, 13, 10, 30);
 
 console.log(date1.toLocaleDateString());
 console.log(date2.toLocaleDateString(), date2.toLocaleTimeString());
+
+let some_date = new Date(month + " " + date + "," + year);
+console.log(some_date);	
+console.log(some_date.toLocaleDateString());
+console.log(some_date.toLocaleTimeString());
