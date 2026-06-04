3075d859ee74a57a260a569c343bc6c758cfc358
Eugene <eugene.gincu@gmail.com>
2026-06-03 23:29:13 -0700

    started weather cli script

---
+++ weather.js
@@ -0,0 +1,5 @@
+"use strict"
+
+let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/calgary?unitGroup=metric /
+			&include=current%2Calerts%2Cdays%2Chours&key=FFME32Y944SAC9E4UNA4MWQAC&contentType=json"
+			
